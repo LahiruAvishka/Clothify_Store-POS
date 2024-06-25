4628b6b5f27d55e237c29c4b89d1898fbfd502f1
LahiruAvishka <wlavishka.95@gmail.com>
2024-06-25 09:16:08 +0530

    first commit

---
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,11 @@
+
+S
+#src/main/java/org/example/Main.java,0\b\0ba1b9c7a4878802f938e331cc2d8a042087ec6b
+7
+pom.xml,4\4\442292b8a7efeabbe4cc176709b833b1792140ec
+f
+6src/main/java/org/example/DashBoardFormController.java,9\4\94e0e4cce883de9502ef73dea8e0eebd7076aac9
+G
+src/main/java/Main.java,e\4\e44a8606ce010ad9b89aafdfcee2ab49afbf6d10
+q
+Asrc/main/java/org/example/controller/DashBoardFormController.java,b\f\bf903a0c2f7b10748620209e76b15b6198809649
---
+++ src/main/java/Main.java
@@ -0,0 +1,18 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+//import org.example.util.HibernateUtil;
+//import org.hibernate.Session;
+
+public class Main extends Application {
+    public static void main(String[] args) {
+        launch();
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("view/dashboard-form.fxml"))));
+        stage.show();
+    }
+}
