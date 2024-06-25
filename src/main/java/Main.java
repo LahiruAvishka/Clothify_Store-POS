f8d0454dd67a9709560d94379072ac1a003e09f9
LahiruAvishka <wlavishka.95@gmail.com>
2024-06-25 18:31:00 +0530

    Update and Modify

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("view/dashboard-form.fxml"))));
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("view/login-form.fxml"))));
         stage.show();
     }
 }
