99d50c41012118dfe5f62bfe52b4b6b465abbd1b
LahiruAvishka <wlavishka.95@gmail.com>
2024-06-25 09:48:49 +0530

    Update and Modify

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,8 +2,8 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-//import org.example.util.HibernateUtil;
-//import org.hibernate.Session;
+import org.example.util.HibernateUtil;
+import org.hibernate.Session;
 
 public class Main extends Application {
     public static void main(String[] args) {
